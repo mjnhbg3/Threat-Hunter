ca545fb16909cf6245022af5ca3d8c1b92754737
mjnhbg3 <dukekesten@gmail.com>
2025-07-23 13:00:13 -0400

    Add batch size and position reset for Wazuh logs

--- threat_hunter/settings.py
+++ threat_hunter/settings.py
@@ -12,7 +12,14 @@ def get_threat_hunter_core() -> ThreatHunterCore:
     wazuh_user = os.environ.get("WAZUH_API_USER", "wazuh")
     wazuh_password = os.environ.get("WAZUH_API_PASSWORD", "wazuh")
     log_file = os.environ.get("WAZUH_LOG_FILE", "/var/ossec/logs/alerts/alerts.json")
+    batch_size = int(os.environ.get("WAZUH_BATCH_SIZE", "1000"))
     db_dir = os.environ.get("THREAT_DB_DIR", "./db")
 
-    wazuh_api = WazuhAPI(wazuh_base_url, wazuh_user, wazuh_password, log_file)
+    wazuh_api = WazuhAPI(
+        wazuh_base_url,
+        wazuh_user,
+        wazuh_password,
+        log_file,
+        batch_size=batch_size,
+    )
     return ThreatHunterCore(api_keys=api_keys, wazuh_api=wazuh_api, db_dir=db_dir)
